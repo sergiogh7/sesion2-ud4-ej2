a4aaf36acffae5f808d720b3d52a6826608f1d97
Sergio <87013586+sergiogh7@users.noreply.github.com>
2021-07-07 09:53:57 +0200

    Sesion2-Ud4-Ej2

---
+++ S2Ud4Ej2App.java
@@ -0,0 +1,25 @@
+/**
+ * Version 1.0
+ * @author Sergio Galimany Hoyo
+ * 
+ */
+public class S2Ud4Ej2App {
+
+	public static void main(String[] args) {
+		
+		// Variables
+		int N = 5;
+        double A = 4.56;
+        char C = 'a';
+        
+        // Printa resultados.
+        System.out.println("Variable N = " + N);
+        System.out.println("Variable A = " + A);
+        System.out.println("Variable C = " + C);
+        System.out.println(N + " + " + A + " = " + (N+A));
+        System.out.println(A + " - " + N + " = " + (A-N));
+        System.out.println("Valor numerico del caracter " + C + " = " + (int)C); 
+
+	}
+
+}
